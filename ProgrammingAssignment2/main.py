5e823f59556df12442dd68029df63ce3fb005a99
Raghavendra <raghavendra.kamma@icloud.com>
2023-06-01 02:00:31 -0700

    handle dict value tuple with 1,2, or 3 values

--- ProgrammingAssignment2/main.py
+++ ProgrammingAssignment2/main.py
@@ -13,6 +13,13 @@ def process_fs(s_domain_func, s_var, limits):
         variable_symbol = Symbol(variable)
         if not isinstance(limit_values, tuple):    # Handle both single numeric value and one-item tuple
             limit_values = (limit_values,)
+        elif len(limit_values) == 1:   # If only one value is given, use it for all three limits.
+            limit_values = (limit_values[0], limit_values[0], limit_values[0]) # Handle different lengths of limit tuples
+        elif len(limit_values) == 2:    # If only two values are given, use the second value for the third limit.
+            limit_values = (limit_values[0], limit_values[1], limit_values[1])
+        elif len(limit_values) == 3:    # If three values are given, use them as is.
+            limit_values = (limit_values[0], limit_values[1], limit_values[2])
+        
         for i, limit_value in enumerate(limit_values):
             num = num.subs(variable_symbol, limit_value)
             den = den.subs(variable_symbol, limit_value)
@@ -28,7 +35,10 @@ def test_func():
     s = Symbol("s")
     p1 = Symbol("p1")
     p2 = Symbol("p2")
+    p3 = Symbol("p3")
     z1 = Symbol("z1")
+    z2 = Symbol("z2")
+    z3 = Symbol("z3")
 
     # sp = 1 / (s * p1) # low pass
     # sp = s / (s * p1 + 1) # high pass
@@ -46,12 +56,28 @@ def test_func():
     # sp = (s + 1) / ((s - 2)*(s + 3)*(s**2 + 2*s + 5))
     # sp = (2 + 42*s)/((1 + 2*s)*(1 + 40*s))
 
-    sp = (s + p1) / (s + p2)
+    # sp = (s + p1) / (s + p2)
+    
+    # sp = (s + 1) / ((s - 2)*(s + 3)*(s**2 + 2*s + 5))
+    # sp = (2 + 42*s)/((1 + 2*s)*(1 + 40*s))
     
     # limits = {"p1": (1e3,)}
     # limits = {"p1": (1e3,), "z1": (1e2, 1e4)} # Second limit example 
-    limits = {"p1": 1e3, "p2": (10e3,)} # limit example to show limits dictionary work with either single numeric value or a one item tuple
+    # limits = {"p1": 1e3, "p2": (10e3,)} # limit example to show limits dictionary work with either single numeric value or a one item tuple
 
+    # limits = {"p1": 1e3}
+    # limits = {"p1": (1e3, 1e4)}
+    # limits = {"p1": (1e3, 5e3, 1e4)}
+
+    # sp = s
+    # limits = {"p1": 1}
+    # sp = (s + z1) / (s + p1)
+    # limits = {"p1": (1e3,), "z1": (1e2,)}
+    # sp = ((s + z1)*(s + z2)) / ((s + p1)*(s + p2))
+    # limits = {"p1": (1e3,), "p2": (10e3,), "z1": (1e2, 1e4), "z2": (1e3, 1e5)}
+    sp = ((s + z1)*(s + z2)*(s + z3)) / ((s + p1)*(s + p2)*(s + p3))
+    limits = {"p1": (1e3,), "p2": (10e3,), "p3": (100e3,), "z1": (1e2, 1e4), "z2": (1e3, 1e5), "z3": (1e4, 1e6)}
+    
     process_fs(sp, s, limits)
 
 if __name__ == "__main__":
